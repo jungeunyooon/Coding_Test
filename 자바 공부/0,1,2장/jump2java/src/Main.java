00d7f765f2a1a0a4ce0e3ad93898f11f8a4e8832
윤정은 <angal2310@tukorea.ac.kr>
2023-11-03 03:37:40 +0900

    Your commit message

---
+++ 자바 공부/0,1,2장/jump2java/src/Main.java
@@ -0,0 +1,26 @@
+/*public class Sample {
+
+    *//* 속성 *//*
+    private String message;
+
+    *//* 생성자 *//*
+    public Sample(String message) {
+        this.message = message;
+    }
+}*/
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+
+/*
+String a = "Happy Java"; // 리터럴 표기 방식
+String a = new String("Happy Java");
+new 키워드는 객체를 만들 때 사용한다.
+원시 자료형 (int, long, double, float, boolean, char)은 new 키워드로 값을 생성할 수 없다
+리터럴 표기 방식으로만 값을 세팅할 수 있다.
+멀티 스레드 환경에서 동기화를 지원하려면 원시 자료형대신 Wrapper 클래스를 사용해야 한다. ( Integer Long Double Float Boolean Character)*/
+
+
